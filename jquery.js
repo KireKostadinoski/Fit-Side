478d928a3ad0b047afc9c5a4b28f7ef6e7ff95e5
Kire Kostadinoski <k.kostadinoski@outlook.com>
2023-09-21 11:38:00 +0200

    Added project Fit  Side

---
+++ jquery.js
@@ -0,0 +1,78 @@
+//home page//
+
+$('.banner1').hover(function(){
+    $('.nas').fadeIn(1000);
+});
+
+$('.banner1').hover(function(){
+    $('.btnsn').fadeIn(1000);
+});
+
+//About us page//
+
+$('.abusbanner').hover(function(){
+    $('.nas').fadeIn(1000);
+});
+
+//Membership page//
+
+$('.membanner').hover(function(){
+    $('.nas').fadeIn(1000);
+});
+
+$('.membanner').hover(function(){
+    $('.btnsn').fadeIn(1000);
+});
+
+$('.membtn').click(function(){
+  $('.card1').slideToggle();
+})
+
+$('.membtn').click(function(){
+    $('.card2').slideToggle();
+})
+
+$('.membtn').click(function(){
+    $('.card3').slideToggle();
+})
+
+//Programs Page//
+
+$('.progbanner').hover(function(){
+    $('.nas').fadeIn(1000);
+});
+
+$('.progbanner').hover(function(){
+    $('.btnsn').fadeIn(1000);
+});
+
+$('.hoverpr').hover(function(){
+    $('.fitnesspr a').fadeIn(2000);
+})
+
+$('.hoverpr').hover(function(){
+    $('.crossfitpr a').fadeIn(2000);
+})
+
+$('.hoverpr').hover(function(){
+    $('.yogapr a').fadeIn(2000);
+})
+
+//Fitness page
+
+$('.col-sm-4').hover(function(){
+    $('.fitnesspr a').fadeIn(2000);
+})
+
+$('.hoverpr').hover(function(){
+    $('.crossfitpr a').fadeIn(2000);
+})
+
+$('.hoverpr').hover(function(){
+    $('.yogapr a').fadeIn(2000);
+})
+
+//Contact page//
+ $('.cusbanner').hover(function(){
+    $('.nas').fadeIn(500);
+ });
